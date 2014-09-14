8f252590ceb9428471b09626940a3184da1b94ae
Shashank Polasa <shashank.pv@gmail.com>
2014-09-14 22:25:13 +0530

    Adding old files

---
+++ 4.py
@@ -0,0 +1,10 @@
+#906609
+ 
+def ispalin(si): return str(si)==str(si)[::-1]
+ 
+aux=0
+for k in range(101,1000):
+    for j in range(101,1000):
+            if ispalin(str(j*k)) and j*k>aux:
+                    aux=j*k
+print aux
