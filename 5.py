105e5bee6cf6b37e8762c72216ed2d1d05744224
Shashank Polasa <shashank.pv@gmail.com>
2014-09-14 22:26:52 +0530

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,19 @@
+#232792560
+#lcm
+ 
+n = 20
+def lcm(a,b):
+  c = a*b
+  while b>0:
+    a,b = b,a%b
+  return c/a
+ 
+l = set()
+for i in xrange(2, n):
+  if i%2 != 0: l.add(2*i)
+  else: l.add(i)
+ 
+c = 1
+for i in l:
+  c = lcm(c,i)
+print c
